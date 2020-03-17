d70bc28229e500b1764e1df50613b54351e18e9b
Your_Name <joseph161216@gmail.com>
2020-03-17 15:20:11 -0700

    update admin configs

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Todo  # add this
+
+class TodoAdmin(admin.ModelAdmin):  # add this
+  list_display = ('title', 'description', 'completed')  # add this
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)  # add this
+
+
+'''
+Todo: Set up Django as an API
+'''
