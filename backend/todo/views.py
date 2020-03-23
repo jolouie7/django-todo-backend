78f16ba9462f0c6fd16f2a24e8dedcbf4fece8ff
Your_Name <joseph161216@gmail.com>
2020-03-22 19:16:07 -0700

    add TodoView class

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
